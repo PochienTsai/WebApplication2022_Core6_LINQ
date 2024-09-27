106c72e81174398bda1f3a2f0c6fabe19d95c6c2
PochienTsai <bernie7135@gmail.com>
2024-09-27 23:15:54 +0800

    加入註解

--- obj/Debug/net6.0/WebApplication2022_Core6_LINQ.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApplication2022_Core6_LINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2022_Core6_LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c5593b17560da3b46e3c24d260ab47bab9e53e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95001ac792e903d2442a04f3c7b567ba8beb91c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2022_Core6_LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2022_Core6_LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
